82c07e17e0aeb3522f50a1c8b0e00bf963be1ae3
mounirchebbi <2157@holbertonschool.com>
2025-04-20 13:45:15 +0100

    lit

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+    if (!startString || typeof startString !== 'string') return '';
+  
+    const filtered = [...set]
+      .filter((value) => typeof value === 'string' && value.startsWith(startString))
+      .map((value) => value.slice(startString.length));
+  
+    return filtered.join('-');
+  }
+  
