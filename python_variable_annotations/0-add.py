8c40855d8fd5ab2ce86c22d2d487ccee8dea2658
mounirchebbi <2157@holbertonschool.com>
2025-07-12 07:52:42 +0100

    basic annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def add(a: float, b: float) -> float:
+    """
+    This function takes two float numbers and returns their sum.
+
+    Parameters:
+        a (float)
+        b (float)
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
